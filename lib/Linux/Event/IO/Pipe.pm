ee5c44a2905f5744598810fd0a1759acfaa5adba
Joshua S. Day <haxmeister@hotmail.com>
2026-09-03 17:38:53 -0500

    Route Pipe through private byte-stream layer

--- lib/Linux/Event/IO/Pipe.pm
+++ lib/Linux/Event/IO/Pipe.pm
@@ -5,7 +5,7 @@ use warnings;
 
 our $VERSION = '0.105';
 
-use parent 'Linux::Event::Stream';
+use parent 'Linux::Event::_ByteStream';
 
 1;
 
@@ -18,8 +18,8 @@ Linux::Event::IO::Pipe - ordered byte I/O over pipe-like handles
 =head1 DESCRIPTION
 
 This class is the public pipe-oriented leaf of the corrected Linux::Event I/O
-taxonomy. It currently delegates to the proven byte-stream implementation while
-that implementation is moved behind private architecture layers.
+taxonomy. Its byte-stream behavior is provided through the private
+L<Linux::Event::_ByteStream> implementation boundary.
 
 A pipe object may have C<read_fh>, C<write_fh>, or both. The two directions may
 use different descriptors. Anonymous pipes, FIFOs, and child-process pipe pairs
