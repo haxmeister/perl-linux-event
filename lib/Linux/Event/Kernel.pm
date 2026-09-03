e04ac274528d3badc5c7652297324286ff7fdaef
Joshua S. Day <haxmeister@hotmail.com>
2026-09-03 17:35:34 -0500

    Add Kernel namespace

---
+++ lib/Linux/Event/Kernel.pm
@@ -0,0 +1,24 @@
+package Linux::Event::Kernel;
+use v5.36;
+use strict;
+use warnings;
+
+our $VERSION = '0.105';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Linux::Event::Kernel - kernel event and state namespace
+
+=head1 DESCRIPTION
+
+C<Linux::Event::Kernel> is a namespace category for Linux::Event abstractions
+over Linux kernel notification and state facilities. Applications choose a
+concrete leaf such as L<Linux::Event::Kernel::Timer>,
+L<Linux::Event::Kernel::Signal>, L<Linux::Event::Kernel::Event>, or
+L<Linux::Event::Kernel::Process>.
+
+=cut
