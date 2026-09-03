929908abb3c8f17d9231fa83b0ebbc70bb8743db
Joshua S. Day <haxmeister@hotmail.com>
2026-09-03 17:34:43 -0500

    Add IO namespace

---
+++ lib/Linux/Event/IO.pm
@@ -0,0 +1,22 @@
+package Linux::Event::IO;
+use v5.36;
+use strict;
+use warnings;
+
+our $VERSION = '0.105';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Linux::Event::IO - application data I/O namespace
+
+=head1 DESCRIPTION
+
+C<Linux::Event::IO> is a namespace category, not a generic I/O object.
+Applications choose a concrete leaf such as L<Linux::Event::IO::Pipe>,
+L<Linux::Event::IO::TTY>, or a socket type below L<Linux::Event::IO::Sock>.
+
+=cut
