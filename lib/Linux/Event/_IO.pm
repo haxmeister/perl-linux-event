be84ec893f0508f8d615f4cd30929ceda5403b43
Joshua S. Day <haxmeister@hotmail.com>
2026-09-03 17:52:59 -0500

    Add shared IO handle validation helper

--- lib/Linux/Event/_IO.pm
+++ lib/Linux/Event/_IO.pm
@@ -5,6 +5,32 @@ use warnings;
 
 our $VERSION = '0.105';
 
+use Carp qw(croak);
+
+sub _constructor_handles ($method, $option) {
+    my $fh = $option->{fh};
+    my $read_fh = $option->{read_fh};
+    my $write_fh = $option->{write_fh};
+
+    croak "$method(): fh cannot be combined with read_fh or write_fh"
+        if defined($fh) && (defined($read_fh) || defined($write_fh));
+    if (defined $fh) {
+        $read_fh = $fh;
+        $write_fh = $fh;
+    }
+
+    my @handle;
+    my %seen;
+    for my $pair ([read_fh => $read_fh], [write_fh => $write_fh]) {
+        next if !defined $pair->[1];
+        my $fd = fileno($pair->[1]);
+        croak "$method(): $pair->[0] must be a filehandle" if !defined $fd;
+        next if $seen{$fd}++;
+        push @handle, [$pair->[0], $pair->[1], $fd];
+    }
+    return @handle;
+}
+
 1;
 
 __END__
@@ -21,5 +47,8 @@ subclassing API and applications must not depend on it.
 Public I/O classes describe completed Linux I/O facilities. Shared descriptor,
 reactor, and lifecycle machinery may be factored through this package while
 remaining invisible to application code.
+
+C<_constructor_handles> is a cold construction helper used by concrete I/O
+leaves to validate the actual Linux facility before generic byte-stream setup.
 
 =cut
