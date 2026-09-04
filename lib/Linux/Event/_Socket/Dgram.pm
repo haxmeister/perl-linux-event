410fdb469a5ed6cf17274ffe7f6873d1ed098996
Joshua S. Day <haxmeister@hotmail.com>
2026-09-03 19:40:04 -0500

    Document stable private datagram boundary

--- lib/Linux/Event/_Socket/Dgram.pm
+++ lib/Linux/Event/_Socket/Dgram.pm
@@ -20,8 +20,13 @@ Linux::Event::_Socket::Dgram - private C<SOCK_DGRAM> implementation boundary
 
 =head1 DESCRIPTION
 
-This package is internal. During migration it bridges the corrected socket
-architecture to the proven datagram implementation. Datagram message boundaries
-remain kernel-provided and do not use the byte-stream framing layer.
+This package is internal. It joins the private socket classification with the
+proven datagram implementation hosted under the historical
+C<Linux::Event::Datagram> package name. That implementation name is private and
+is not an application subclassing API.
+
+Datagram message boundaries remain kernel-provided and do not use the
+ordered-byte framing layer. The public leaf is
+L<Linux::Event::IO::Sock::Dgram>.
 
 =cut
