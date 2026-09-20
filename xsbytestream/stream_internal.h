fff04fa6fef7f91bb0bb9049a62d9e25997f758a
Joshua S. Day <haxmeister@hotmail.com>
2026-09-19 21:27:43 -0500

    Use save-stack compatible consumer call depth

--- xsbytestream/stream_internal.h
+++ xsbytestream/stream_internal.h
@@ -171,7 +171,7 @@ typedef struct les_xsstate_s {
     const les_consumer_ops_v1_t *consumer_next_ops;
     void *consumer_next_context;
     UV consumer_host_retain_count;
-    unsigned int consumer_call_depth;
+    int consumer_call_depth;
     int consumer_transition_pending;
     int destroy_pending;
 
